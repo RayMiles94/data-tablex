4522c46f6be1acbb5e13a13545763cf8e2dbd31b
RaoufJabriSAMSA-IT <Raouf.Jabri@samsa-it.de>
2020-10-01 16:30:17 +0100

    [ADD] price react hook

--- src/components/main/pricing/pricing.jsx
+++ src/components/main/pricing/pricing.jsx
@@ -1,15 +1,58 @@
-import React,{ Component } from "react";
+import React, { useState } from "react";
 import "./pricing.css";
 
-export default class Princing extends Component {
+export default function Princing() {
 
-    constructor(props) {
-        super(props);
-        this.state= {};
-    }
+    const [price, setprice] = useState({
+        prices: [
+            {
+                name: 'server1',
+                price: '10$'
+            },
+            {
+                name: 'server2',
+                price: '10$'
+            },
+            {
+                name: 'server3',
+                price: '20$'
+            },
+            {
+                name: 'server4',
+                price: '30$'
+            }
+        ]
+    })
 
-    render() {
-        document.title='TableX charts | princing';
-        return(<div className="princing">Princing</div>)
-    }
+    document.title = 'TableX charts | princing';
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Princing</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <table className="table table-sm">
+                        <tr>
+                            <td>Server</td>
+                            <td>Price</td>
+                        </tr>
+                        {
+                            price.prices.map(  e => {
+                                return (
+                                    <tr id={e.name}>
+                                        <td>{e.name}</td>
+                                        <td>{e.price} </td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    );
 }
